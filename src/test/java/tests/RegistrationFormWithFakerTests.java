a8fbf91db86f9d7f5ade30e251d03f83d64bd5ba
AnastasiaF <kachanav1009@gmail.com>
2023-11-28 23:02:18 +0300

    added WithTestDataTests and WithFakerTests

--- src/test/java/tests/RegistrationFormWithFakerTests.java
+++ src/test/java/tests/RegistrationFormWithFakerTests.java
@@ -1,60 +1,51 @@
 package tests;
 
 
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
-import java.util.Locale;
+import static tests.TestData.*;
 
-import static tests.TestDate.*;
 
 public class RegistrationFormWithFakerTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
+    TestData data = new TestData();
 
 
     @Test
     void fillFormTest() {
 
-        Faker faker = new Faker(new Locale("eng"));
-
-
-        String firstName = faker.name().firstName();
-        String lastName = faker.name().lastName();
-        String userEmail = faker.internet().emailAddress();
-        String streetAddress = faker.address().streetAddress();
 
         //заполнение всех полей
+
         registrationPage.openPage()
                 .setFirstName(firstName)
                 .setLastName(lastName)
                 .setEmail(userEmail)
-                .setGender(userGender)
-                .setUserNumber(userNumber)
-                .setDateOfBirth("10", "September", "1992")
-                .setSubjects(subjects)
-                .setHobbies(hobbyFirst)
-                .setHobbies(hobbySecond)
-                .setHobbies(hobbyThird)
+                .setGender(data.gender)
+                .setUserNumber(data.userNumber)
+                .setDateOfBirth(data.day, data.month, data.year)
+                .setSubjects(data.subject)
+                .setHobbies(data.hobbies)
                 .uploadPicture()
-                .setCurrentAddress(streetAddress)
-                .setState("Uttar Pradesh")
-                .setCity("Lucknow")
+                .setCurrentAddress(data.streetAddress)
+                .setState(data.state)
+                .setCity(data.city)
                 .pressSubmit();
 
 
         //Проверка
-        registrationPage.checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Student Email", userEmail)
-                .checkResult("Gender", userGender)
-                .checkResult("Mobile", userNumber)
-                .checkResult("Date of Birth", "10 September,1992")
-                .checkResult("Subjects", subjects)
-                .checkResult("Hobbies", hobbyFirst + ", " + hobbySecond + ", " + hobbyThird)
+        registrationPage.checkResult("Student Name", data.firstName + " " + data.lastName)
+                .checkResult("Student Email", data.userEmail)
+                .checkResult("Gender", data.gender)
+                .checkResult("Mobile", data.userNumber)
+                .checkResult("Date of Birth", data.day + " " + data.month + "," + data.year)
+                .checkResult("Subjects", data.subject)
+                .checkResult("Hobbies", data.hobbies + ", " + data.hobbies + ", " + data.hobbies)
                 .checkResult("Picture", "PictureForTest.png")
-                .checkResult("Address", streetAddress)
-                .checkResult("State and City", state + " " + city);
+                .checkResult("Address", data.streetAddress)
+                .checkResult("State and City", data.state + " " + data.city);
     }
 
     @Test
@@ -62,17 +53,17 @@ public class RegistrationFormWithFakerTests extends TestBase {
         //заполнение обязательных полей
 
         registrationPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setGender(userGender)
-                .setEmail(userEmail)
-                .setUserNumber(userNumber)
+                .setFirstName(data.firstName)
+                .setLastName(data.lastName)
+                .setGender(data.gender)
+                .setEmail(data.userEmail)
+                .setUserNumber(data.userNumber)
                 .pressSubmit();
 
-        registrationPage.checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Student Email", userEmail)
-                .checkResult("Gender", userGender)
-                .checkResult("Mobile", userNumber);
+        registrationPage.checkResult("Student Name", data.firstName + " " + data.lastName)
+                .checkResult("Student Email", data.userEmail)
+                .checkResult("Gender", data.gender)
+                .checkResult("Mobile", data.userNumber);
 
     }
 
@@ -80,9 +71,9 @@ public class RegistrationFormWithFakerTests extends TestBase {
     void negativeTest() {
 
         registrationPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setGender(userGender)
+                .setFirstName(data.firstName)
+                .setLastName(data.lastName)
+                .setGender(data.gender)
                 .pressSubmit()
                 .checkNotCompleteForm();
 
